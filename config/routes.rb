0e47fbc1006056f5b20dbbe84d1dc8dc6782aeea
kouuuki <mark.web.888@gmail.com>
2017-10-24 17:05:42 +0900

    deviseの導入及び、初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   resources :images
   resources :prototypes
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'prototypes#index'
 end
